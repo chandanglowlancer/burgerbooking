e23c2fcb12f6af6c5aa51ecfab441db44d904f61
chandanglowtech <50921978+chandanglowtech@users.noreply.github.com>
2024-09-20 13:53:42 +0530

    index

---
+++ script.js
@@ -0,0 +1,8 @@
+gsap.from('div', {oppacity:0 , duration : 1} )
+
+gsap.from('.section-2', {oppacity:0 , duration : 2, y: -100} )
+gsap.from('nav', {oppacity:0 , duration : 2, y: -100} )
+gsap.from('.section-1 p', {oppacity:0 , duration : 1, y: -100} )
+gsap.from('.section-1 h1', {oppacity:0 , duration : 1.2, y: -100} )
+gsap.from('.section-1 img', {oppacity:0 , duration : 1.4, y: -100} )
+gsap.from('.section-1 button', {oppacity:0 , duration : 1.6, y: -100} )
